1fc87a7716bab6185f0f5c88959b34ab9137b4bf
CBIS2021Claus <98592074+CBIS2021Claus@users.noreply.github.com>
2022-02-18 13:40:03 +0100

    Actualizar main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -3,14 +3,42 @@ bluetooth.startLEDService()
 let degrees = input.compassHeading()
 basic.showString("COMPASS")
 basic.forever(function () {
-    degrees = input.compassHeading()
     if (degrees < 45 || degrees > 315) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
         basic.showString("N")
     } else if (degrees < 135) {
         basic.showString("E")
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
     } else if (degrees < 225) {
         basic.showString("S")
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
     } else {
         basic.showString("W")
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
     }
+    degrees = input.compassHeading()
 })
